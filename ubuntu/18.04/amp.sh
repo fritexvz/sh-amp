5759d64c8d75865e81eb944c86941c9ccb5e606c
www <git@w3labs.dev>
2020-04-15 00:02:45 +0900

    The ftpusers file has been added to vsftpd's restore configuration settings.

--- ubuntu/18.04/amp.sh
+++ ubuntu/18.04/amp.sh
@@ -214,6 +214,7 @@ openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout /etc/ssl/private/vs
 
 printf "\n\nCreate a file to restore configuration settings of vsftpd ... \n"
 copy "/etc/vsftpd.conf"
+copy "/etc/ftpusers"
 copy "/etc/vsftpd.user_list"
 copy "/etc/vsftpd.chroot_list"
 
