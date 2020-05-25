8feb0e2ad47cb531532c157bf80329e631f69d90
www <git@w3labs.dev>
2020-05-25 16:44:08 +0900

    npm installation was added.

---
+++ ubuntu/18.04/npm/install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+# Download and run the latest release version.
+# https://github.com/w3src/sh-amp
+#
+# Usage
+# git clone https://github.com/w3src/sh-amp.git
+# cd sh-amp
+# chmod +x ./ubuntu/18.04/npm/install.sh
+# ./ubuntu/18.04/npm/install.sh
+
+# Work even if somebody does "sh thisscript.sh".
+set -e
+
+# Set constants.
+OSPATH="$(dirname "$(dirname $0)")"
+PKGPATH="$(dirname $0)"
+PKGNAME="$(basename "$(dirname $0)")"
+FILENAME="$(basename $0)"
+
+# Set directory path.
+ABSROOT="$(cd "$(dirname "")" && pwd)"
+ABSENV="${ABSROOT}/env"
+ABSOS="${ABSROOT}/${OSPATH}"
+ABSPKG="${ABSOS}/${PKGNAME}"
+ABSPATH="${ABSPKG}/${FILENAME}"
+
+# Include the file.
+source "${ABSOS}/utils.sh"
+source "${ABSOS}/functions.sh"
+source "${ABSPKG}/functions.sh"
+
+echo
+echo "Start installing ${PKGNAME^^}."
+
+apt -y install npm
+
+# Reloading the service.
+systemctl reload apache2
+
+echo
+echo "${PKGNAME^^} is completely installed."
+
