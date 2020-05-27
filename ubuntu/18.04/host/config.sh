d35cf38aeead4291a57b704a1c6b376e8057d87a
www <git@w3labs.dev>
2020-05-27 17:00:41 +0900

    A host configuration file has been added.

---
+++ ubuntu/18.04/host/config.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+#
+# Download and run the latest release version.
+# https://github.com/w3src/sh-amp
+#
+# Usage
+# git clone https://github.com/w3src/sh-amp.git
+# cd sh-amp
+# chmod +x ./ubuntu/18.04/host/install.sh
+# ./ubuntu/18.04/host/install.sh
+
+# Work even if somebody does "sh thisscript.sh".
+set -e
+
+# Set constants.
+OSPATH="$(dirname "$(dirname $0)")"
+PKGPATH="$(dirname $0)"
+PKGNAME="$(basename "$(dirname $0)")"
+FILENAME="$(basename $0)"
+
+# Set directory path.
+ABSROOT="$(cd "$(dirname "")" && pwd)"
+ABSENV="${ABSROOT}/env"
+ABSOS="${ABSROOT}/${OSPATH}"
+ABSPKG="${ABSOS}/${PKGNAME}"
+ABSPATH="${ABSPKG}/${FILENAME}"
+
+# Include the file.
+source "${ABSOS}/utils.sh"
+source "${ABSOS}/functions.sh"
+source "${ABSPKG}/functions.sh"
+
+echo
+echo "Set the host name."
+
+# Set the arguments.
+for arg in "${@}"; do
+  case $arg in
+  --my)
+    IFS=$'\n'
+    for i in $(find "${ABSPKG}/etc" -type f -name "[^_]*"); do
+      cp "$i" "$(echo "$i" | sed "s/${ABSPKG//\//\\/}//")"
+    done
+    echo "${PKGNAME^} configuration is complete."
+    exit 0
+    ;;
+  esac
+done
+
+# Make sure the hostname is changed.
+CHANGE_MESSAGE="$(msg -yn "Do you want to change host name? (y/n) ")"
+
+if [ "${CHANGE_MESSAGE}" == "No" ]; then
+  exit 0
+fi
+
+# Set the host name
+HOST_NAME="$(msg -yn -c "hostname or FQDN (ex) example.com : ")"
+
+if [ hostname != "${HOST_NAME}" ]; then
+  hostnamectl set-hostname "${HOST_NAME}"
+fi
+
+if [ -z "$(cat /etc/hosts | egrep "127.0.1.1 ${HOST_NAME}")" ]; then
+  sed -i -e "1 a\127.0.1.1 ${HOST_NAME}" /etc/hosts
+fi
+
+# This will cause the set and update hostname module to not operate (if true)
+sed -i -E -e '/preserve_hostname\s{0,}\:/{ s/\:.*/\: true/; }' /etc/cloud/cloud.cfg
+
+echo
+echo "Host name has been set."
--- ubuntu/18.04/host/install.sh
+++ ubuntu/18.04/host/install.sh
@@ -33,49 +33,14 @@ source "${ABSPKG}/functions.sh"
 echo
 echo "Set the host name."
 
-# Import variables from the env file.
-PUBLIC_IP="$(getPubIPs)"
+# Create backup and configuration files.
+addPkgCnf "/etc/hosts"
+addPkgCnf "/etc/cloud/cloud.cfg"
 
 # Add a variable to the env file.
-addPkgCnf -rs="\[HOSTS\]" -fs="=" -o="<<HERE
-PUBLIC_IP = ${PUBLIC_IP}
+setPkgCnf -rs="\[HOSTS\]" -fs="=" -o="<<HERE
+PUBLIC_IP = $(getPubIPs)
 <<HERE"
-
-# Set the host name
-CHANGE_MESSAGE="$(msg -yn "Do you want to change host name? (y/n) ")"
-if [ "${CHANGE_MESSAGE}" == "No" ]; then
-  exit 0
-fi
-
-# Set the host name
-HOST_NAME="$(msg -yn -c "hostname or FQDN (ex) example.com : ")"
-
-if [ hostname != "${HOST_NAME}" ]; then
-  hostnamectl set-hostname "${HOST_NAME}"
-fi
-
-# Create a backup file.
-cp -v /etc/hosts{,.bak}
-cp -v /etc/cloud/cloud.cfg{,.bak}
-
-f_hosts="/etc/hosts"
-
-if [ -f ".${f_hosts}" ]; then
-  cp -v ".${f_hosts}" "${f_hosts}"
-else
-  if [ -z "$(cat "${f_hosts}" | grep "127.0.1.1 ${HOST_NAME}")" ]; then
-    sed -i -e "1 a\127.0.1.1 ${HOST_NAME}" "${f_hosts}"
-  fi
-fi
-
-# This will cause the set+update hostname module to not operate (if true)
-f_cloud="/etc/cloud/cloud.cfg"
-
-if [ -f ".${f_cloud}" ]; then
-  cp -v ".${f_cloud}" "${f_cloud}"
-else
-  sed -i -E -e '/preserve_hostname\s{0,}\:/{ s/\:.*/\: true/; }' "${f_cloud}"
-fi
 
 echo
 echo "Host name has been set."
